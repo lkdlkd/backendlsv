6540e2bb4300ce63661b4e947ce151f6a673058c
lkdlkd <khanhdang2440@gmail.com>
2025-07-11 19:16:49 +0700

    23

--- controllers/tool/Updatetocdo.js
+++ controllers/tool/Updatetocdo.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const Service = require('../../models/server');
 
 // Cronjob: cập nhật tốc độ dự kiến cho tất cả dịch vụ mỗi 10 phút
-cron.schedule('*/5 * * * *', async () => {
+cron.schedule('*/30 * * * *', async () => {
   console.log('Cronjob: Bắt đầu cập nhật tốc độ dự kiến cho tất cả dịch vụ...');
   const results = await Service.updateAllTocDoDuKien();
   console.log('Kết quả cập nhật tốc độ:', results);
