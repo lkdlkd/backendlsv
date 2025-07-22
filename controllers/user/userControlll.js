06739d9ced01688b5877d801024affd15485b149
lkdlkd <khanhdang2440@gmail.com>
2025-07-22 22:14:02 +0700

    44

--- controllers/user/userControlll.js
+++ controllers/user/userControlll.js
@@ -22,8 +22,12 @@ exports.login = async (req, res) => {
     }
 
     // Lưu lịch sử đăng nhập vào mảng loginHistory
-    const ip = (req.headers['x-forwarded-for'] || '').split(',')[0].trim() || null;
-     const userAgent = req.headers['user-agent'] || '';
+    // Ưu tiên lấy IP từ header X-User-IP (IP thật từ client), sau đó mới dùng x-forwarded-for
+    const ip = req.headers['x-user-ip'] ||
+      (req.headers['x-forwarded-for'] || '').split(',')[0].trim() ||
+      req.connection.remoteAddress ||
+      null;
+    const userAgent = req.headers['user-agent'] || '';
     user.loginHistory = user.loginHistory || [];
     user.loginHistory.push({ ip, agent: userAgent, time: new Date() });
     await user.save();
